6da4ab8006a2dbde174ae37c0c4b5239d9c1db84
Gorm Lai <723464+gormlai@users.noreply.github.com>
2023-10-18 11:55:22 +0100

    setup simple export function

--- src/v-hacd_lib.cpp
+++ src/v-hacd_lib.cpp
@@ -1,3 +1,13 @@
 #define ENABLE_VHACD_IMPLEMENTATION 1
 #include <VHACD.h>
 
+#include <string>
+
+extern "C" {
+  void printNumber(int num);
+}
+
+void printNumber(int num) {
+  const std::string numS = std::to_string(num);
+  printf("num is %s\n", numS.c_str());
+}
