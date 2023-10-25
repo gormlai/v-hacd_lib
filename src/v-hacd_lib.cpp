e5dff7ca00326c497b5a42cf1015ad04bb61f0c5
Gorm Lai <723464+gormlai@users.noreply.github.com>
2023-10-25 15:02:35 +0100

    work in progress on returning an array of convex hulls

--- src/v-hacd_lib.cpp
+++ src/v-hacd_lib.cpp
@@ -5,9 +5,38 @@
 
 extern "C" {
   void printNumber(int num);
+  int convexDecomposition(const float * vertices, const uint32_t * indices, const unsigned int numVertices, const unsigned int numIndices);
 }
 
 void printNumber(int num) {
   const std::string numS = std::to_string(num);
   printf("num is %s\n", numS.c_str());
+}
+
+int convexDecomposition(const float * vertices, const uint32_t * indices, const unsigned int numVertices, const unsigned int numIndices) {
+  VHACD::IVHACD * cDecomposer = VHACD::CreateVHACD();
+  assert(cDecomposer != nullptr);
+
+  //  double *dVertices = new double[numVertices*3];
+  double dVertices[numVertices*3];
+  for(unsigned int vertexIndex = 0 ; vertexIndex < (numVertices*3) ; vertexIndex++) {
+    dVertices[vertexIndex] = vertices[vertexIndex];
+  }
+  VHACD::IVHACD::Parameters parameters;
+  const bool decomposeResult = cDecomposer->Compute(dVertices, numVertices, indices, numIndices / 3, parameters);
+  if(decomposeResult) {
+    const int numConvexHulls = cDecomposer->GetNConvexHulls();
+
+    for(int convexHullIndex = 0 ; convexHullIndex < numConvexHulls ; convexHullIndex++) {
+      VHACD::IVHACD::ConvexHull convexHull;
+      cDecomposer->GetConvexHull(convexHullIndex, convexHull);
+    }
+
+    return numConvexHulls;    
+  }
+  else {
+    printf("Failed to calculate convex hull\n");
+    return 0;
+  }
+
 }
